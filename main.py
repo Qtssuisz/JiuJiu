c0845d648cfdb54c729361d3f351279335b252a3
Qtssuisz <87359471+Qtssuisz@users.noreply.github.com>
2022-08-27 22:59:38 +0800

    Update main.py

--- main.py
+++ main.py
@@ -19,12 +19,11 @@ user_id_ = "oU1cX6H0njPkQao84BPmbNF5kS90"
 template_id = os.environ["TEMPLATE_ID"]
 remainder = """\n
 #############\n
-7点健身\n
+5点健身\n
 好好和我谈恋爱\n
 #############"""
 """
 健身
-周六八点
 周日八点
 """
 """
